b7aaab181e5fab6f56ac55365b164c89ca9cc436
TanteElli <46008725+TanteElli@users.noreply.github.com>
2018-12-22 23:34:11 +0100

    Add files via upload

--- biest2.ino
+++ biest2.ino
@@ -11,6 +11,15 @@
 
 //Legs Movement
 
+#define MFL 12  // Move Front Left
+#define MBL 14   // Move Back Left
+#define MML 13  // Move Middle Left
+
+#define MFR 17   // Move Front Right
+#define MBR 16   // Move Back Right
+#define MMR 15   // Move Middle Right
+
+
 //Head
 #define HT 8   // Tilt
 #define HR 2   // Pan Right
@@ -29,6 +38,14 @@ void setup() {
   pinMode(FR, OUTPUT);
   pinMode(BR, OUTPUT);
   pinMode(MR, OUTPUT);
+
+  pinMode(MFL, OUTPUT);
+  pinMode(MBL, OUTPUT);
+  pinMode(MML, OUTPUT);
+  pinMode(MFR, OUTPUT);
+  pinMode(MBR, OUTPUT);
+  pinMode(MMR, OUTPUT);
+
   pinMode(HT, OUTPUT);
   pinMode(HR, OUTPUT);
   pinMode(HL, OUTPUT);
@@ -89,14 +106,84 @@ void tap_dance(int dly) {
 }
 
 
-void loop() {
-    look_around();
-    tap_dance(300);
-    tap_dance(200);
-    tap_dance(100);
-    tap_dance(50);
-    tap_dance(50);
-    tap_dance(50);
-    tap_dance(50);    
+void move_fwd() {
+  digitalWrite(FR, HIGH);
+  delay(100);
+  digitalWrite(MFR, LOW);
+  delay(500);
+  digitalWrite(FR, LOW);
+  delay(500);
 
+  digitalWrite(FL, HIGH);
+  delay(100);
+  digitalWrite(MFL, LOW);
+  delay(500);
+  digitalWrite(FL, LOW);
+  delay(500);
+
+  digitalWrite(MR, HIGH);
+  delay(100);
+  digitalWrite(MMR, LOW);
+  delay(500);
+  digitalWrite(MR, LOW);
+  delay(500);
+
+  digitalWrite(ML, HIGH);
+  delay(100);
+  digitalWrite(MML, LOW);
+  delay(500);
+  digitalWrite(ML, LOW);
+  delay(500);
+
+  digitalWrite(BR, HIGH);
+  delay(100);
+  digitalWrite(MBR, LOW);
+  delay(500);
+  digitalWrite(BR, LOW);
+  delay(500);
+
+  digitalWrite(BL, HIGH);
+  delay(100);
+  digitalWrite(MBL, LOW);
+  delay(500);
+  digitalWrite(BL, LOW);
+  delay(500);
+  
+ 
+  digitalWrite(MFR, HIGH);
+  digitalWrite(MMR, HIGH);
+  digitalWrite(MBR, HIGH);
+  digitalWrite(MFL, HIGH);
+  digitalWrite(MML, HIGH);
+  digitalWrite(MBL, HIGH);
+  
+  delay(1000);
+  
+}
+
+void loop() {
+
+  look_around();
+  
+  //head up, middle pos
+  digitalWrite(HT, HIGH);
+  delay(500);
+
+  digitalWrite(HR, HIGH);  
+
+  for (int a=0;a<=5;a++) {
+    move_fwd();
+  }
+
+  //tail up
+  digitalWrite(TL, HIGH);
+    
+  //wait 15 Sek
+  delay(15000);
+
+  //tail down
+  digitalWrite(TL, LOW);
+
+  //wait 5 Sek
+  delay(5000);
 }
